a0630028b85334e5b8c841a2af529ae3ce61dd0b
cherrylife7 <44017822+cherrylife7@users.noreply.github.com>
2020-03-17 18:24:06 +0300

    Add project files.

---
+++ TestEPAM/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestEPAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+
+
+        }
+    }
+}
